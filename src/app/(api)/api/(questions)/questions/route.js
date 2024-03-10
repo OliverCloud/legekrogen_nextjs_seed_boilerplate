d01fd968a2e27f0e8ae6400791b155b1968c919f
Anders Christensen <anders@medieskolerne.dk>
2024-03-10 22:24:53 +0100

    init

---
+++ src/app/(api)/api/(questions)/questions/route.js
@@ -0,0 +1,9 @@
+import { getQuestions } from "@/lib/data.service";
+import { NextResponse } from "next/server"
+
+export async function GET() {
+
+    let questions = await getQuestions();
+    return NextResponse.json(questions);
+
+}
